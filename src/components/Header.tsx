d440391d5d3a0572475811a8d98d03cd8d26d836
John Freitas <john.freitasau@gmail.com>
2021-04-29 07:21:48 +1000

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import { Flex, Text, Input, Icon } from "@chakra-ui/react";
+import {
+  RiSearchLine,
+  RiNotificationLine,
+  RiUserAddLine,
+} from "react-icons/ri";
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      align="center"
+      px="6"
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        dashgo
+        <Text as="span" color="pink.500">
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          placeholder="What are you looking for?"
+          _placeholder={{ color: "gray.400" }}
+        />
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <Icon as={RiNotificationLine} fontSize="20" />
+        <Icon as={RiUserAddLine} fontSize="20" />
+      </Flex>
+    </Flex>
+  );
+}
