bcf909e130fb4b97b885570beea7a534b8486f32
John Freitas <john.freitasau@gmail.com>
2021-05-11 07:52:42 +1000

    Started implementation of react-hook-form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,13 +4,17 @@ import {
   FormControl,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, ...rest },
+  ref
+) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -24,8 +28,11 @@ export function Input({ name, label, ...rest }: InputProps) {
         _hover={{ bgColor: "gray.900" }}
         size="lg"
         mt=""
+        ref={ref}
         {...rest}
       />
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Flex, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm<FormData>();
+
+  const { errors } = formState;
+
+  function handleSignIn(values) {
+    console.log(values);
+  }
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,10 +26,21 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="Email" />
-          <Input name="password" type="password" label="Password" />
+          <Input
+            name="email"
+            type="email"
+            label="Email"
+            {...register("email", { required: "Email is required." })}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Password"
+            {...register("password")}
+          />
         </Stack>
         <Button type="submit" mt="6" colorScheme="pink" size="lg">
           Enter
